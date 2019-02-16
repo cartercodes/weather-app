e91c664df2e2d00f279f300ce73991f22b3253c7
Mitchell Carter <carter.svo@gmail.com>
2019-02-15 23:58:32 -0500

    updating package.jsom for heroku push

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from "../src/components/App";
+import App from "./components/App";
 import './style.css';
 require('./ably');
 
